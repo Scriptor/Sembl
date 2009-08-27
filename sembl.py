2f89ba21772791d508127ccb102e52345813d4cf
Tamreen <historium@gmail.com>
2009-08-27 02:25:46 -0400

    Added support for type checking and compiler errors

--- sembl.py
+++ sembl.py
@@ -7,13 +7,37 @@ plus = lambda stack: stack.pop() + stack.pop()
 minus = lambda stack: -stack.pop() + stack.pop()
 mul = lambda stack: stack.pop() * stack.pop()
 div = lambda stack: stack.pop(1) / stack.pop()
+
 words = {
-	'+': plus,
-	'-': minus,
-	'*': mul,
-	'/': div
+	'+': (plus, ('number', 'number'), ('number',)),
+	'-': (minus, ('number', 'number'), ('number',)),
+	'*': (mul, ('number', 'number'), ('number',)),
+	'/': (div, ('number', 'number'), ('number',)),
 }
 
+class CompileError(Exception):
+	def __init__(self, msg):
+		self.msg = msg
+	def __str__(self):
+		return self.msg
+	
+def typecheck(tok, typecode):
+	if words.has_key(tok):
+		inputs = words[tok][1]
+		products = words[tok][2]
+		for i,expected_typ in enumerate(reversed(inputs)):
+			if not len(typecode):
+				typecode.append("null")
+
+			if expected_typ != typecode[-1]:
+				raise CompileError("Expecting %s, got %s" % (expected_typ, typecode[-1]))
+			else:
+				typecode.pop()
+		typecode.extend(products)
+			
+	else:
+		typecode.append(typeof(tok))
+	
 def lex(code):
 	class Counter():
 		def __init__(self, s):
@@ -33,7 +57,10 @@ def lex(code):
 	
 def compile(toks):
 	bytecode = []
+	typecode = []
+	
 	for tok in toks:
+		typecheck(tok, typecode)
 		bytecode.append(typify(tok))
 	return bytecode
 	
@@ -41,13 +68,19 @@ def execute(bytecode):
 	stack = []
 	for term in bytecode:
 		if words.has_key(term):
-			stack.append(words[term](stack))
+			stack.append(words[term][0](stack))
 		else:
 			stack.append(term)
 	return stack
 
 code = """
-	3 4 /
+	2 3 +
+	3 *
 """
 toks = lex(code)
-print execute(compile(toks))
+try:
+	bytecode = compile(toks)
+except CompileError as err:
+	print "   Compile error:\n\t %s\n" % err
+else:
+	print execute(compile(toks))
