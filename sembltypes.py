bf10ba6b4e6c0040767e5caca90b7c88f73d9d62
Tamreen <historium@gmail.com>
2009-08-27 14:21:23 -0400

    Added block type

--- sembltypes.py
+++ sembltypes.py
@@ -2,27 +2,33 @@ import re
 pats = {
 	'int': r"^[-+]?[0-9]+$",
 	'float': r"^[-+]?[0-9]*\.?[0-9]+$",
+	'block': r"^__block[0-9]+__$",
 	'word': r'^.+$'
 }
 
 def number(tok):
 	if re.match(pats['float'], tok):
 		return float(tok)
-	else:
-		return False
+	return False
+
+def block(tok):
+	if re.match(pats['block'], tok)
+		return tok
+	return False
+	
+def null(tok):
+	if tok == "null":
+		return tok
+	return False
 
 def word(tok):
 	if re.match(pats['word'], tok):
 		return tok
 	return False
 
-def null(tok):
-	if tok == "null":
-		return tok
-	return False
-
 type_funcs = (
 	('number', number),
+	('block', block),
 	('null', null),
 	('word', word)
 )
